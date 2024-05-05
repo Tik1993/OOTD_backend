78f7dbded6145efc4d91e1b39dace35e955eb212
Ernest Lin <linchongtik@gmail.com>
2024-05-04 23:28:51 -0700

    create Item routes

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,5 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const Item = require("../models/Item");
+const Category = require("../models/Category");
+const Subcategory = require("../models/Subcategory");
 
 // @desc Get all Items
 // @route GET /items
@@ -13,4 +15,57 @@ const getAllItems = asyncHandler(async (req, res) => {
   res.json(items);
 });
 
-module.exports = { getAllItems };
+// @desc Create a new Item
+// @route POST /items
+// @access Private
+const createItem = asyncHandler(async (req, res) => {
+  const {
+    name,
+    categoryName,
+    subcategoryName,
+    size_available,
+    length_available,
+    color_available,
+    price,
+  } = req.body;
+
+  if (
+    !name ||
+    !price ||
+    !categoryName ||
+    !subcategoryName ||
+    !size_available ||
+    !color_available
+  ) {
+    return res.status(400).json({
+      message:
+        "Please provide a name, price, category, subcategory, size_available, color_available",
+    });
+  }
+
+  const category = await Category.findOne({ name: categoryName });
+  if (!category) {
+    return res.status(404).json({ message: "Category not found" });
+  }
+
+  const subcategory = await Category.findOne({ name: subcategoryName });
+  if (!subcategory) {
+    return res.status(404).json({ message: "Subategory not found" });
+  }
+  const item = new Item({
+    name,
+    price,
+    category,
+    subcategory,
+    size_available,
+    length_available,
+    color_available,
+  });
+  try {
+    await item.save();
+    res.status(201).json(item);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+module.exports = { getAllItems, createItem };
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const itemsController = require("../controllers/itemsController");
 
-router.get("/", itemsController.getAllItems);
+router
+  .get("/", itemsController.getAllItems)
+  .post("/", itemsController.createItem);
 
 module.exports = router;
