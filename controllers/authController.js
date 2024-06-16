065c548eac22241f1a2f735902ba50f329db584d
Ernest Lin <linchongtik@gmail.com>
2024-06-15 21:50:59 -0700

    implement login controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,47 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 // @desc login user
 // @route POST /auth/login
 // @access Public
 const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide a username and password" });
+  }
+  const user = await User.findOne({ username }).lean();
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const matchPassword = await bcrypt.compare(password, user.password);
+  if (!matchPassword) {
+    return res.status(401).json({ message: "Invalid credentials" });
+  }
+
+  const accessToken = jwt.sign(
+    { UserInfo: { username: user.username, roles: user.roles } },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: "15m" }
+  );
+
+  const rehreshToken = jwt.sign(
+    { username: user.username },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: "7d" }
+  );
+
+  res.cookie("refreshToken", rehreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
   console.log("login");
-  res.send("login");
+  return res.status(200).json({ accessToken });
 });
 
 // @desc refresh user
